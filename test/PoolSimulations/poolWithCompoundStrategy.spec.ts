f6c0ec668b32bf1a1857453d36d1d3744712a1ba
Akshay Meher <akshay_111meher@hotmail.com>
2021-08-04 20:28:42 +0530

    test/PoolSimulations/poolWithCompoundStrategy.spec.ts

--- test/PoolSimulations/poolWithCompoundStrategy.spec.ts
+++ test/PoolSimulations/poolWithCompoundStrategy.spec.ts
@@ -47,7 +47,7 @@ import { getContractAddress } from '@ethersproject/address';
 import { SublimeProxy } from '@typechain/SublimeProxy';
 import { IYield } from '@typechain/IYield';
 
-describe.only('Pool With Compound Strategy', async () => {
+describe('Pool With Compound Strategy', async () => {
     let savingsAccount: SavingsAccount;
     let savingsAccountLogic: SavingsAccount;
 
