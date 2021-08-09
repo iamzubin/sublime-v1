f34fb741423a282d0d2153073e26f81feeff77ee
KalpitaMandal <kalpita1703@gmail.com>
2021-08-09 23:59:40 +0530

    New Base Code

--- test/PoolSimulations/poolWithComp2.spec.ts
+++ test/PoolSimulations/poolWithComp2.spec.ts
@@ -1,4 +1,4 @@
-import { createEnvironment } from '../../utils/createEnv';
+import { calculateNewPoolAddress, createEnvironment, createNewPool } from '../../utils/createEnv';
 import {
     CompoundPair,
     CreditLineDefaultStrategy,
@@ -24,7 +24,12 @@ import {
     testPoolFactoryParams,
 } from '../../utils/constants';
 
-describe('Pool With Compound Strategy 2', async () => {
+import DeployHelper from '../../utils/deploys';
+import { ERC20 } from '../../typechain/ERC20';
+import { sha256 } from '@ethersproject/sha2';
+import { BigNumber } from 'ethers';
+
+describe.only('Pool With Compound Strategy 2', async () => {
     let env: Environment;
     before(async () => {
         env = await createEnvironment(
@@ -65,6 +70,43 @@ describe('Pool With Compound Strategy 2', async () => {
     });
 
     it('Sample', async function () {
-        console.log(env);
+        let salt = sha256(Buffer.from(`borrower-${new Date().valueOf()}`));
+        let { admin, borrower, lender } = env.entities;
+        let deployHelper: DeployHelper = new DeployHelper(admin);
+        let DAI: ERC20 = await deployHelper.mock.getMockERC20(Contracts.DAI);
+        let WBTC: ERC20 = await deployHelper.mock.getMockERC20(Contracts.WBTC);
+
+        let poolAddress = await calculateNewPoolAddress(env, DAI, WBTC, env.yields.compoundYield, salt, false, {
+            _poolSize: BigNumber.from(100).mul(BigNumber.from(10).pow(18)),
+            _minborrowAmount: BigNumber.from(10).mul(BigNumber.from(10).pow(18)),
+            _borrowRate: BigNumber.from(1).mul(BigNumber.from(10).pow(28)),
+            _collateralAmount: BigNumber.from(1).mul(BigNumber.from(10).pow(8)),
+            _collateralRatio: BigNumber.from(250).mul(BigNumber.from(10).pow(28)),
+            _collectionPeriod: 10000,
+            _matchCollateralRatioInterval: 200,
+            _noOfRepaymentIntervals: 100,
+            _repaymentInterval: 1000,
+        });
+
+        console.log({ calculatedPoolAddress: poolAddress });
+
+        console.log(env.mockTokenContracts[1].name);
+        await env.mockTokenContracts[1].contract.connect(env.impersonatedAccounts[0]).transfer(admin.address, '100000000');
+        await env.mockTokenContracts[1].contract.connect(admin).transfer(borrower.address, '100000000');
+        await env.mockTokenContracts[1].contract.connect(borrower).approve(poolAddress, '100000000');
+
+        let pool = await createNewPool(env, DAI, WBTC, env.yields.compoundYield, salt, false, {
+            _poolSize: BigNumber.from(100).mul(BigNumber.from(10).pow(18)),
+            _minborrowAmount: BigNumber.from(10).mul(BigNumber.from(10).pow(18)),
+            _borrowRate: BigNumber.from(1).mul(BigNumber.from(10).pow(28)),
+            _collateralAmount: BigNumber.from(1).mul(BigNumber.from(10).pow(8)),
+            _collateralRatio: BigNumber.from(250).mul(BigNumber.from(10).pow(28)),
+            _collectionPeriod: 10000,
+            _matchCollateralRatioInterval: 200,
+            _noOfRepaymentIntervals: 100,
+            _repaymentInterval: 1000,
+        });
+
+        console.log({ actualPoolAddress: pool.address });
     });
 });
