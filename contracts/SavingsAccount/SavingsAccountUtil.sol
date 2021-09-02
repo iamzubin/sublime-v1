d969ccbbda92d6782031cd613b2316a70ef64d1b
Prateek Reddy <prateekreddyy@gmail.com>
2021-09-02 16:44:38 +0530

    0 ether transfer avoided

--- contracts/SavingsAccount/SavingsAccountUtil.sol
+++ contracts/SavingsAccount/SavingsAccountUtil.sol
@@ -111,10 +111,10 @@ library SavingsAccountUtil {
                 (bool success, ) = payable(_to).call{value: _amount}('');
                 require(success, 'Transfer failed');
             }
-            if (msg.value >= _amount) {
+            if (msg.value > _amount) {
                 (bool success, ) = payable(address(msg.sender)).call{value: msg.value - _amount}('');
                 require(success, 'Transfer failed');
-            } else {
+            } else if(msg.value != _amount) {
                 revert('Insufficient Ether');
             }
             return _amount;
