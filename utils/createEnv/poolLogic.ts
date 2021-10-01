c6628ecd2620328e65335305b20c63d458591859
Prateek Reddy <prateekreddyy@gmail.com>
2021-10-01 19:11:37 +0530

    PoolLogic and poolTokenLogic are behind proxy

--- utils/createEnv/poolLogic.ts
+++ utils/createEnv/poolLogic.ts
@@ -3,9 +3,12 @@ import { SignerWithAddress } from '@nomiclabs/hardhat-ethers/signers';
 import DeployHelper from '../deploys';
 
 import { Pool } from '@typechain/Pool';
+import { SublimeProxy } from '@typechain/SublimeProxy';
 
 export async function createPool(proxyAdmin: SignerWithAddress): Promise<Pool> {
     let deployHelper: DeployHelper = new DeployHelper(proxyAdmin);
-    let pool: Pool = await deployHelper.pool.deployPool();
+    let poolLogic: Pool = await deployHelper.pool.deployPool();
+    let poolProxy: SublimeProxy = await deployHelper.helper.deploySublimeProxy(poolLogic.address, proxyAdmin.address);
+    let pool: Pool = await deployHelper.pool.getPool(poolProxy.address);
     return pool;
 }
--- utils/createEnv/poolToken.ts
+++ utils/createEnv/poolToken.ts
@@ -3,9 +3,12 @@ import { SignerWithAddress } from '@nomiclabs/hardhat-ethers/signers';
 import DeployHelper from '../deploys';
 
 import { PoolToken } from '@typechain/PoolToken';
+import { SublimeProxy } from '@typechain/SublimeProxy';
 
 export async function createPoolToken(proxyAdmin: SignerWithAddress): Promise<PoolToken> {
     let deployHelper: DeployHelper = new DeployHelper(proxyAdmin);
-    let poolToken: PoolToken = await deployHelper.pool.deployPoolToken();
+    let poolTokenLogic: PoolToken = await deployHelper.pool.deployPoolToken();
+    let poolTokenProxy: SublimeProxy = await deployHelper.helper.deploySublimeProxy(poolTokenLogic.address, proxyAdmin.address);
+    let poolToken: PoolToken = await deployHelper.pool.getPoolToken(poolTokenProxy.address);
     return poolToken;
 }
