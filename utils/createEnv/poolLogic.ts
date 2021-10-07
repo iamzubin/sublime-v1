993e86a679bb7fae612f53020ab1232b37b369f1
Prateek Reddy <prateekreddyy@gmail.com>
2021-10-07 13:23:42 +0530

    Revert "PoolLogic and poolTokenLogic are behind proxy"
    This reverts commit c6628ecd2620328e65335305b20c63d458591859.

--- utils/createEnv/poolLogic.ts
+++ utils/createEnv/poolLogic.ts
@@ -3,12 +3,9 @@ import { SignerWithAddress } from '@nomiclabs/hardhat-ethers/signers';
 import DeployHelper from '../deploys';
 
 import { Pool } from '@typechain/Pool';
-import { SublimeProxy } from '@typechain/SublimeProxy';
 
 export async function createPool(proxyAdmin: SignerWithAddress): Promise<Pool> {
     let deployHelper: DeployHelper = new DeployHelper(proxyAdmin);
-    let poolLogic: Pool = await deployHelper.pool.deployPool();
-    let poolProxy: SublimeProxy = await deployHelper.helper.deploySublimeProxy(poolLogic.address, proxyAdmin.address);
-    let pool: Pool = await deployHelper.pool.getPool(poolProxy.address);
+    let pool: Pool = await deployHelper.pool.deployPool();
     return pool;
 }
--- utils/createEnv/poolToken.ts
+++ utils/createEnv/poolToken.ts
@@ -3,12 +3,9 @@ import { SignerWithAddress } from '@nomiclabs/hardhat-ethers/signers';
 import DeployHelper from '../deploys';
 
 import { PoolToken } from '@typechain/PoolToken';
-import { SublimeProxy } from '@typechain/SublimeProxy';
 
 export async function createPoolToken(proxyAdmin: SignerWithAddress): Promise<PoolToken> {
     let deployHelper: DeployHelper = new DeployHelper(proxyAdmin);
-    let poolTokenLogic: PoolToken = await deployHelper.pool.deployPoolToken();
-    let poolTokenProxy: SublimeProxy = await deployHelper.helper.deploySublimeProxy(poolTokenLogic.address, proxyAdmin.address);
-    let poolToken: PoolToken = await deployHelper.pool.getPoolToken(poolTokenProxy.address);
+    let poolToken: PoolToken = await deployHelper.pool.deployPoolToken();
     return poolToken;
 }
