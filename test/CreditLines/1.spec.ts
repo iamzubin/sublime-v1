e2d55a31c136e060f74f2b3a18731979b274f840
Prateek Reddy <prateekreddyy@gmail.com>
2021-10-27 12:07:32 +0530

    fix tests

--- test/CreditLines/1.spec.ts
+++ test/CreditLines/1.spec.ts
@@ -229,7 +229,7 @@ describe('Credit Lines', async () => {
         });
 
         it('Check global variables', async () => {
-            expect(await creditLine.CreditLineCounter()).to.eq(0);
+            expect(await creditLine.creditLineCounter()).to.eq(0);
             expect(await creditLine.strategyRegistry()).to.eq(strategyRegistry.address);
             expect(await creditLine.defaultStrategy()).to.eq(yearnYield.address);
         });
@@ -429,7 +429,7 @@ describe('Credit Lines', async () => {
 
             it('Check global variables', async () => {
                 // TODO: check all global variables
-                expect(await creditLine.CreditLineCounter()).to.eq(0);
+                expect(await creditLine.creditLineCounter()).to.eq(0);
                 expect(await creditLine.strategyRegistry()).to.eq(strategyRegistry.address);
                 expect(await creditLine.defaultStrategy()).to.eq(yearnYield.address);
             });
