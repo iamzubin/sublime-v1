63a4354efd273924bd8733ba269dba584bff607f
KalpitaMandal <kalpita1703@gmail.com>
2022-01-12 00:31:39 +0530

    Adding the correct comments in creditline.sol

--- contracts/CreditLine/CreditLine.sol
+++ contracts/CreditLine/CreditLine.sol
@@ -95,7 +95,7 @@ contract CreditLine is ReentrancyGuard, OwnableUpgradeable {
 
     /**
      * @notice stores the fraction of borrowed amount charged as fee by protocol
-     * @dev it is multiplied by 10**30
+     * @dev it is multiplied by 10**30 (precision)
      **/
     uint256 public protocolFeeFraction;
 
@@ -106,7 +106,7 @@ contract CreditLine is ReentrancyGuard, OwnableUpgradeable {
 
     /**
      * @notice stores the fraction of amount liquidated given as reward to liquidator
-     * @dev it is multiplied by 10**30
+     * @dev it is multiplied by 10**30 (precision)
      **/
     uint256 public liquidatorRewardFraction;
     /**
@@ -375,8 +375,9 @@ contract CreditLine is ReentrancyGuard, OwnableUpgradeable {
 
     /**
      * @dev Used to Calculate Interest Per second on given principal and Interest rate
-     * @param _principal principal Amount for which interest has to be calculated.
+     * @param _principal principal Amount for which interest has to be calculated
      * @param _borrowRate It is the Interest Rate at which Credit Line is approved
+     * @param _timeElapsed It is the time interval in seconds for which interest is calculated
      * @return interest per second for the given parameters
      */
     function calculateInterest(
