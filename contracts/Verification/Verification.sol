af1031e7b5f264c23e011eabc9c5c298c6ddcc08
saxenism <rahulsaxena.pro@gmail.com>
2022-01-11 12:23:48 +0530

    removed superflous check

--- contracts/Verification/Verification.sol
+++ contracts/Verification/Verification.sol
@@ -79,12 +79,12 @@ contract Verification is Initializable, IVerification, OwnableUpgradeable {
     /// @dev unregistering master address doesn't affect linked addreses mapping to master address, though they would not be verified by this verifier anymore
     /// @param _masterAddress address which is being unregistered
     /// @param _verifier verifier address from which master address is unregistered
-    function unregisterMasterAddress(address _masterAddress, address _verifier) external override {
+    function unregisterMasterAddress(address _masterAddress) external override {
         if (msg.sender != super.owner()) {
-            require(masterAddresses[_masterAddress][msg.sender] || msg.sender == _verifier, 'V:UMA-Invalid verifier');
+            require(masterAddresses[_masterAddress][msg.sender] != 0, 'V:UMA-Invalid verifier');
         }
-        delete masterAddresses[_masterAddress][_verifier];
-        emit UserUnregistered(_masterAddress, _verifier, msg.sender);
+        delete masterAddresses[_masterAddress][msg.sender];
+        emit UserUnregistered(_masterAddress, msg.sender, msg.sender);
     }
 
     /// @notice Link an address with a master address
