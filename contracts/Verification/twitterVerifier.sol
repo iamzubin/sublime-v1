f691189ddc38cae9401b6a73fcee6c4dd18fcd11
iamzubin <zchoudhary.10@gmail.com>
2022-02-23 20:54:40 +0530

    fix issues

--- contracts/Verification/twitterVerifier.sol
+++ contracts/Verification/twitterVerifier.sol
@@ -15,7 +15,14 @@ contract TwitterVerifier is Initializable, IVerifier, OwnableUpgradeable {
      * @notice stores the user metadata against their address
      */
     mapping(address => string) public userData;
+    /**
+     * @notice mapping from userData to userAddress
+     */
     mapping(string => address) public twitterIdMap;
+    /**
+     * @notice stores used hashes for signed message
+     */
+    mapping(byte32 => address) private hashAddressMap;
     address public signerAddress;
 
     /**
@@ -23,11 +30,17 @@ contract TwitterVerifier is Initializable, IVerifier, OwnableUpgradeable {
      * @param verification address of the updated verification contract
      */
     event VerificationUpdated(address indexed verification);
+     /**
+     * @notice emitted when Signer address is updated
+     * @param signerAddress address of the updated verification contract
+     */
     event SignerUpdated(address indexed signerAddress);
 
     /// @notice Initializes the variables of the contract
     /// @dev Contract follows proxy pattern and this function is used to initialize the variables for the contract in the proxy
     /// @param _admin Admin of the verification contract who can add verifiers and remove masterAddresses deemed invalid
+    /// @param _verification Verification contract address
+    /// @param _signerAddress Address of the signer bot who'll verify twitter account and sign messages off-chain
     function initialize(
         address _admin,
         address _verification,
@@ -41,12 +54,12 @@ contract TwitterVerifier is Initializable, IVerifier, OwnableUpgradeable {
 
     /**
      * @notice used to register user
-     * @dev ohly owner can register users
+     * @dev only owner can register users
      * @param _v int v
      * @param _r part signed message hash
      * @param _s part signed message hash
-     * @param _deadline deadline for the signed message
-     * @param _twitterId metadata related to the user
+     * @param _timestamp timestamp for the signed message
+     * @param _userData metadata related to user :  here "twitterId/tweetId"
      * @param _isMasterLinked should master address be linked to itself
      */
 
@@ -55,66 +68,73 @@ contract TwitterVerifier is Initializable, IVerifier, OwnableUpgradeable {
         uint8 _v,
         bytes32 _r,
         bytes32 _s,
-        string memory _twitterId,
-        uint256 _deadline
+        string memory _userData,
+        uint256 _timestamp
     ) external {
         require(bytes(userData[msg.sender]).length == 0, 'User already exists');
-        require(block.timestamp < _deadline, 'Signed transaction expired');
+        require(twitterIdMap[_userData], 'Signed message already used');
+        require(block.timestamp < _timestamp + 86400, 'Signed transaction expired');
         bytes32 eip712DomainHash = keccak256(
             abi.encode(keccak256('EIP712Domain(string name,string version)'), keccak256(bytes('SublimeTwitter')), keccak256(bytes('1')))
         );
 
         bytes32 hashStruct = keccak256(
             abi.encode(
-                keccak256('set(string twitterId,address userAddr,uint256 deadline)'),
-                keccak256(bytes(_twitterId)),
+                keccak256('set(string twitterId,address userAddr,uint256 timestamp)'),
+                keccak256(bytes(_userData)),
                 msg.sender,
-                _deadline
+                _timestamp
             )
         );
+        require(bytes(hashAddressMap[hashStruct]).length == 0, 'Hash Already Used');
 
         bytes32 hash = keccak256(abi.encodePacked('\x19\x01', eip712DomainHash, hashStruct));
         address signer = ecrecover(hash, _v, _r, _s);
-        require(signer == signerAddress, 'MyFunction: invalid signature');
+        require(signer == signerAddress, 'Invalid signature');
 
         verification.registerMasterAddress(msg.sender, _isMasterLinked);
-        userData[msg.sender] = _twitterId;
-        twitterIdMap[_twitterId] = msg.sender;
-        emit UserRegistered(msg.sender, _isMasterLinked, _twitterId);
+        userData[msg.sender] = _userData;
+        twitterIdMap[_userData] = msg.sender;
+        emit UserRegistered(msg.sender, _isMasterLinked, _userData);
     }
     
     /**
      * @notice used to unregister user
-     * @dev ohly owner can unregister users
+     * @dev only owner can unregister users
      */
     function unregisterSelf() external {
-        require(bytes(userData[msg.sender]).length != 0, 'User doesnt exists');
-        delete twitterIdMap[userData[msg.sender]];
-        delete userData[msg.sender];
+        string _userdata = userData[msg.sender];
+        require(bytes(_userdata).length != 0, 'User doesnt exists');
+        delete twitterIdMap[_userdata];
+        delete _userdata;
         verification.unregisterMasterAddress(msg.sender, address(this));
         emit UserUnregistered(msg.sender);
     }
 
     function unregisterUser(address _user) external onlyOwner {
         delete userData[_user];
+        delete twitterIdMap[_userdata];
         verification.unregisterMasterAddress(_user, address(this));
         emit UserUnregistered(msg.sender);
     }
 
     /**
      * @notice used to update verification contract address
-     * @dev ohly owner can update
+     * @dev only owner can update
      * @param _verification address of the verification contract
      */
     function updateVerification(address _verification) external onlyOwner {
         _updateVerification(_verification);
     }
-
     function _updateVerification(address _verification) internal {
         verification = IVerification(_verification);
         emit VerificationUpdated(_verification);
     }
-
+    /**
+     * @notice used to update signer address
+     * @dev only owner can update
+     * @param _signerAddress address of the verification contract
+     */
     function updateSignerAddress(address _signerAddress) external onlyOwner {
         _updateSignerAddress(_signerAddress);
     }
